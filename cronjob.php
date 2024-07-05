667ab2258eb024496e70f16bca952f234e2c3ac0
chloe <github@addy.lobatz.de>
2024-07-05 09:08:41 +0200

    added comments and a readme.md

--- cronjob.php
+++ cronjob.php
@@ -1,9 +1,12 @@
 <?php
+//set the table where the data should be safed 
 $table = "travelynx";
+//connect to the database
 require ("mysqli_connect.php");
-
+//download the history.csv
 $history = shell_exec("curl -s -b cookies.txt https://travelynx.de/history.csv");
 
+//escape everything surrounded by double quotes 
 $history = preg_replace_callback('/([^"]*)("((""|[^"])*)"|$)/s', function ($matches) {
     $str = "";
     if (isset($matches[3])) {
@@ -11,19 +14,21 @@ $history = preg_replace_callback('/([^"]*)("((""|[^"])*)"|$)/s', function ($matc
         $str = str_replace("\n", "\rN", $str);
         $str = str_replace('""', "\rQ", $str);
         $str = str_replace(',', "\rC", $str);
-
     }
     return preg_replace('/\r\n?/', "\n", $matches[1]) . $str;
 }, $history);
-unset($str);
+unset($str); //$str is no longer needed so we destroy it
+//take each row out of the csv string and add them to a array
 $history = str_getcsv($history, "\n");
-
+//remove the first row containing only headings
 array_shift($history);
 
 array_walk(
     $history,
     function (&$row) {
+        //add every field an a row to an array
         $row = str_getcsv($row, ",");
+        //all the conversion is done, so we can return the escaped caracters to a readable format
         $row = str_replace("\rC", ',', $row);
         $row = str_replace("\rQ", '"', $row);
         $row = str_replace("\rN", "\n", $row);
@@ -31,11 +36,12 @@ array_walk(
         return $row;
     }
 );
-unset($row);
-
+unset($row); //$row is no longer needed so we destroy it
+//create a string of placeholders for the query 
 $place_holders = implode(',', array_fill(1, 13, '?'));
+//prepare the query 
 $query = $conn->prepare("INSERT IGNORE INTO `$table` VALUES ($place_holders)");
-
+//add each journey to the database 
 foreach ($history as $journey) {
     $query->execute($journey);
 }
---
+++ readme.md
@@ -0,0 +1,16 @@
+# php script for automatically export travelynx journeys to a mysql database 
+
+the script can be run as a cronjob 
+
+normally it should not produce a output 
+
+## dependencies 
+- php 8.1+ with enabled mysqli extension 
+- curl 
+
+## installation
+1. rename `mysql_userdata_example.php` to `mysql_userdata.php` and fill the required fields
+2. either create a table named `travelynx` or change `$tablename` to the table where the data should be stored
+3. export your cookies for [travelynx.de](https://travelynx.de) as a netscape cookies.txt file and place it in the same directory as `cronjob.php` 
+4. either run `php cronjob.php` or add it to your crontab
+> The script must be run in the directory where the `cookies.txt` is located so `cd` into it before running 
